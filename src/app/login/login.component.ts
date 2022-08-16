70b293975ec04616f7ca98c0d8e31b3c97b98857
JhansiKandukuri <kjhansirani20@gmail.com>
2022-08-16 16:40:21 +0530

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+/**
+ * Modify the login component and the login template to collect login details and add the validators as necessary
+ */
+import { AuthenticationService } from '../services/authentication.service';
+
+@Component({
+  templateUrl: 'login.component.html',
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
+
+  ngOnInit() {
+    // setup the loginform and validators
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['',[Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnDestroy() {}
+
+  onSubmit() {
+    //
+  }
+
+  // implement the username validator. Min 6 characters and no digits, special chars
+  usernameValidator() {
+    return false;
+  }
+
+  // implement the password validator
+  // Min 1 uppercase, 1 lower case and a digit. Total length >= 8
+  passwordValidator() {
+    return false;
+  }
+}
